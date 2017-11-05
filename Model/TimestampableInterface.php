301769087838212ade2add0e73a0cd1287f32bb5
Fabián JL <fabian.main@gmail.com>
2017-11-05 01:11:48 -0600

    #1 Both methods are included in HasTimestamps trait from Laravel core

--- Model/TimestampableInterface.php
+++ Model/TimestampableInterface.php
@@ -21,17 +21,7 @@ interface TimestampableInterface
     public function getCreatedAt(): ?\DateTimeInterface;
 
     /**
-     * @param \DateTimeInterface|null $createdAt
-     */
-    public function setCreatedAt(?\DateTimeInterface $createdAt);
-
-    /**
      * @return \DateTimeInterface|null
      */
     public function getUpdatedAt(): ?\DateTimeInterface;
-
-    /**
-     * @param \DateTimeInterface|null $updatedAt
-     */
-    public function setUpdatedAt(?\DateTimeInterface $updatedAt);
 }
